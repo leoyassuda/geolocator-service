273407e620ce8b50c787221c2b2db661345e1ccd
Leo Yassuda <leo.yassuda@gmail.com>
2020-02-10 22:35:00 -0300

    First commit

---
+++ src/main/java/com/leoyas/geolocatorservice/GeolocatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.leoyas.geolocatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeolocatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeolocatorServiceApplication.class, args);
+	}
+
+}
